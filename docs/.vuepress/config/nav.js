3086a5434c083883e9c2e33e8a9e5831643e9bce
Lijianguo19 <627935@ky-tech.com.cn>
2023-03-23 16:01:22 +0800

    feat:文档目录修改

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -6,8 +6,9 @@ module.exports = [
     items: [
       { text: "产品简介", link: "/pages/69fb10/" },
       { text: "快速入门", link: "/pages/58ae2f/" },
-      { text: "安装部署", link: "/pages/cfdc4b/" },
-      { text: "操作指南", link: "/pages/8f1364/" },
+      { text: "安装部署", link: "/pages/3afb01/" },
+      { text: "用户手册", link: "/pages/8f1364/" },
+      { text: "开发手册", link: "/pages/e5dc37/" },
       { text: "联系我们", link: "/pages/794c63/" },
     ]
   },
